87586c34431adf6d214fdd00d3d54944e48c7cf1
risa <risa.mystudy@gmail.com>
2021-10-22 00:24:08 +0000

    kadai01

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,60 +1,95 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-    
-    def index
-        @tasks = Task.all
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit]
+  
+  def index
+
+    if logged_in?
+      @task = current_user.tasks.build
+#      @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc)
     end
-    
-    def show
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+   # @task = Task.new(task_params)
+   # 
+   # if @task.save
+   #   flash[:success] = 'Task が正常に登録されました'
+   #  redirect_to @task
+   #else
+   #   flash.now[:danger] = 'Task が登録されませんでした'
+   #   render :new
+   # end
+   @task = current_user.tasks.build(task_params)
+    if @task.save
+     flash[:success] = 'タスクが正常に登録されました'
+     redirect_to root_url
+    else
+      flash.now[:danger] = 'タスクの登録に失敗しました'
+      render 'tasks/index'
     end
-    
-    def new
-        @task = Task.new
-    end
-    
-    def create
-        @task = Task.new(task_params)
-        
-        if @task.save
-            flash[:success] = 'Task が正常に登録されました'
-            redirect_to @task
-        else
-            flash.now[:danger] = 'Task が登録されませんでした'
-            render :new
-        end
-    end
-    
-    def edit
-    end
-    
-    def update
-        if @task.update(task_params)
-            flash[:success] = 'Task が正常に更新されました'
-            redirect_to @task
-        else
-            flash.now[:danger] = 'Task が更新されませんでした'
-            render :edit
-        end
-    end
-    
-    def destroy
-        @task.destroy
-        
-        flash[:success] = 'タスクは正常に削除されました'
-        redirect_to tasks_url
-    end
-    
+  end
+  
+  def edit
+  end
+  
+  def update
+      if @task.update(task_params)
+          flash[:success] = 'Task が正常に更新されました'
+          redirect_to @task
+      else
+          flash.now[:danger] = 'Task が更新されませんでした'
+          render :edit
+      end
+  end
+  
+  def destroy
+      @task.destroy
+      
+      flash[:success] = 'タスクは正常に削除されました'
+      redirect_to tasks_url
+  end
+  
 #ストロングパラメータなどのprivateメソッドは、Controllerクラス内に記述
-    private
-    
-    #共通化
-    def set_task
+  private
+  
+  #共通化
+  def set_task
+#      mytask_ids = current_user.tasks.pluck(:id)
+#      # = [1,2,5,6,8]
+#  
+#      
+#      #current_user.tasks.each do |task|
+#      #  if task.id == params[:id]
+#      #end
+##binding.pry      
+#      if mytask_ids.blank? || mytask_ids.exclude?(params[:id])
+#        redirect_to root_url
+#     else
         @task = Task.find(params[:id])
+#      end
+  end
+  
+  #Strong Parameter
+  def task_params
+      params.require(:task).permit(:content, :status)
+  end
+
+  #自分の投稿か確認して、違う場合リダイレクトする
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
-    
-    #Strong Parameter
-    def task_params
-        params.require(:task).permit(:content, :status)
-    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_055207) do
+ActiveRecord::Schema.define(version: 2021_10_20_121802) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_10_20_055207) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
