a309b82145180de64490b843a73f9f75100500fa
risa <risa.mystudy@gmail.com>
2021-10-23 07:33:08 +0000

    kadai03

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-    validates :content, presence: true, length: { maximum: 255 }
-    validates :status, presence: true, length: { maximum: 50 }
+    validates :content, length: { maximum: 255 }
+    validates :status, length: { maximum: 10 }
     
     belongs_to :user
 end
